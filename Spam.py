2755bdfc59c7c65e0a2dc9c7fc83d28b22b473cd
Guru Rahul <gururahul123456789@gmail.com>
2024-12-31 00:25:02 +0530

    2nd update

--- Spam.py
+++ Spam.py
@@ -27,3 +27,14 @@ for i in range(0, len(messages)):
     review = [stemmer.stem(word) for word in review if not word in stopwords.words('english')]# Removing the stopwords
     review = ' '.join(review)#Joining the words by the space
     corpus.append(review)
+
+from sklearn.feature_extraction.text import CountVectorizer #CountVectorizer is used to count the words
+cv = CountVectorizer(max_features=3500) #Maximum features are 3500
+X = cv.fit_transform(corpus).toarray()
+y = pd.get_dummies(messages['LABEL'])
+print('xYxYxY',X)
+y=y.iloc[:,1].values
+print("XXXXX",X)
+print("YYYY",y)
+import pickle
+pickle.dump(cv,open('cv-transform.pkl','wb'))
