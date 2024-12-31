3ff48446fedc59e8db97faa656af177605927bdb
Guru Rahul <gururahul123456789@gmail.com>
2024-12-31 15:25:52 +0530

    prototype commit

--- Spam.py
+++ Spam.py
@@ -33,8 +33,28 @@ cv = CountVectorizer(max_features=3500) #Maximum features are 3500
 X = cv.fit_transform(corpus).toarray()
 y = pd.get_dummies(messages['LABEL'])
 print('xYxYxY',X)
-y=y.iloc[:,1].values
+y=y.iloc[:,1].values #1st column is the spam column The main use of iloc is select the specific
 print("XXXXX",X)
 print("YYYY",y)
 import pickle
 pickle.dump(cv,open('cv-transform.pkl','wb'))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+mnb = MultinomialNB(alpha=0.8)
+mnb.fit(X_train, y_train)
+y_pred = mnb.predict(X_test)
+print('Accuracy:', accuracy_score(y_test, y_pred)*100,"%")
+
+pickle.dump(mnb,open('mnb-model.pkl','wb'))
+message = "Hello, how are you doing today?"
+data = [message]
+vect = cv.transform(data).toarray()
+my_prediction = mnb.predict(vect)
+if my_prediction[0] == 1:
+    print("Spam")
+else:
+    print("Not Spam")
